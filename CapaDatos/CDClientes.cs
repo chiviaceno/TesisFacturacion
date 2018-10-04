35418172d342ef0206cd707e268cdc775aebad18
DENNIS ORELLANA <chiviaceno_1990@hotmail.com>
2018-10-03 22:31:31 -0500

    CRUD  para clientes

---
+++ CapaDatos/CDClientes.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace CapaDatos
+{
+    public class CDClientes
+    {
+        private CDConexion conexion = new CDConexion();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand command = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+           
+            command.Connection = conexion.AbrirConexion();
+            command.CommandText = "MostrarCliente";
+            command.CommandType = CommandType.StoredProcedure;
+            leer = command.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+
+            return tabla;
+
+        }
+
+        // capturamos los datos de la capa entidad para correr la sentencia sql procedure
+
+        public void Insertar(CapaEntidad.CECliente cECliente)
+        {
+            command.Connection = conexion.AbrirConexion();
+            command.CommandText = "InsertarCliente";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@nombre", cECliente.Nombre);
+            command.Parameters.AddWithValue("@apellido", cECliente.Apellido);
+            command.Parameters.AddWithValue("@cedula", cECliente.Cedula);
+            command.Parameters.AddWithValue("@direccion", cECliente.Dirrecion);
+            command.Parameters.AddWithValue("@telefono", cECliente.Telefono);
+            command.Parameters.AddWithValue("@email", cECliente.Email);
+            command.Parameters.AddWithValue("@fecha", cECliente.Fecha);
+            command.Parameters.AddWithValue("@estado", cECliente.Estado);
+            command.ExecuteNonQuery();
+            conexion.CerrarConexion();
+            command.Parameters.Clear();
+
+        }
+
+        public void Editar(CapaEntidad.CECliente cECliente )
+        {
+            command.Connection = conexion.AbrirConexion();
+            command.CommandText = "ModificarCliente";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@nombre", cECliente.Nombre);
+            command.Parameters.AddWithValue("@apellido", cECliente.Apellido);
+            command.Parameters.AddWithValue("@cedula", cECliente.Cedula);
+            command.Parameters.AddWithValue("@direccion", cECliente.Dirrecion);
+            command.Parameters.AddWithValue("@telefono", cECliente.Telefono);
+            command.Parameters.AddWithValue("@email", cECliente.Email);
+            command.Parameters.AddWithValue("@fecha", cECliente.Fecha);
+            command.Parameters.AddWithValue("@estado", cECliente.Estado);
+            command.Parameters.AddWithValue("@id", cECliente.Id);
+            command.ExecuteNonQuery();
+            conexion.CerrarConexion();
+            command.Parameters.Clear();
+        }
+
+        public void Eliminar(CapaEntidad.CECliente cECliente)
+        {
+            command.Connection = conexion.AbrirConexion();
+            command.CommandText = "EliminarCliente";
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@id", cECliente.Id);
+
+            command.ExecuteNonQuery();
+
+            command.Parameters.Clear();
+        }
+    }
+}
--- CapaPresentacion/Clientes.cs
+++ CapaPresentacion/Clientes.cs
@@ -8,20 +8,19 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocio;
+using CapaEntidad;
 
 namespace CapaPresentacionAdministracion
 {
     public partial class Clientes : Form
     {
-        public Clientes()
-        {
-            InitializeComponent();
-        }
+        private CNCliente objetoCN = new CNCliente();
+        private CECliente objetoCE = new CECliente();
+        private string IdCliente = null;
+        private Boolean Editar = false;
 
-        private void Clientes_Load(object sender, EventArgs e)
-        {
-
-        }
+        #region MANEJO DEL FORMULARIO
         private void IconMinimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
@@ -43,5 +42,124 @@ namespace CapaPresentacionAdministracion
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
+        #endregion
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void Clientes_Load(object sender, EventArgs e)
+        {
+            MostrarCliente();
+        }
+        private void MostrarCliente()
+        {
+            CNCliente objeto = new CNCliente();
+            dataGridView1.DataSource = objeto.MostrarCliente();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // INSERTAR EMPLEADO
+            if (Editar == false)
+            {
+                try
+                {
+                    objetoCE.Nombre = txtNombre.Text;
+                    objetoCE.Apellido = txtApellido.Text;
+                    objetoCE.Cedula = txtCedula.Text;
+                    objetoCE.Dirrecion = txtDireccion.Text;
+                    objetoCE.Telefono = txtTelefono.Text;
+                    objetoCE.Email = txtEmail.Text;
+                    objetoCE.Fecha = dtmFecha.Text;
+                    objetoCE.Estado = cbxEstado.Text;
+                    objetoCN.InsertarCliente(objetoCE);
+                    MessageBox.Show("Se Ingreso correctamente el Cliente");
+                    MostrarCliente();
+                    LimpiarForm();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se puede Ingresar el Cliente por  " + ex);
+                }
+
+                // EDITAR EMPLEADO
+            }
+            if (Editar == true)
+            {
+                {
+                    try
+                    {
+                        objetoCE.Id = Convert.ToInt32(IdCliente);
+                        objetoCE.Apellido = txtApellido.Text;
+                        objetoCE.Cedula = txtCedula.Text;
+                        objetoCE.Dirrecion = txtDireccion.Text;
+                        objetoCE.Telefono = txtTelefono.Text;
+                        objetoCE.Email = txtEmail.Text;
+                        objetoCE.Fecha = dtmFecha.Text;
+                        objetoCE.Estado = cbxEstado.Text;
+                        objetoCN.EditarCliente(objetoCE);
+                        MessageBox.Show("Se Editó correctamente el Cliente");
+                        MostrarCliente();
+                        LimpiarForm();
+                        Editar = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("No se puede Editar el Cliente por  " + ex);
+                    }
+
+                }
+
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Editar = true;
+                txtNombre.Text = dataGridView1.CurrentRow.Cells["CLI_NOMBRE"].Value.ToString();
+                txtApellido.Text = dataGridView1.CurrentRow.Cells["CLI_APLELLIDO"].Value.ToString();
+                txtCedula.Text = dataGridView1.CurrentRow.Cells["CLI_CEDULA"].Value.ToString();
+                txtDireccion.Text = dataGridView1.CurrentRow.Cells["CLI_DIRECCION"].Value.ToString();
+                txtTelefono.Text = dataGridView1.CurrentRow.Cells["CLI_TELEFONO"].Value.ToString();
+                txtEmail.Text = dataGridView1.CurrentRow.Cells["CLI_EMAIL"].Value.ToString();
+                dtmFecha.Text = dataGridView1.CurrentRow.Cells["CLI_FECHA_REGISTRO"].Value.ToString();
+                cbxEstado.Text = dataGridView1.CurrentRow.Cells["CLI_ESTADO"].Value.ToString();
+                IdCliente = dataGridView1.CurrentRow.Cells["CLI_ID"].Value.ToString();
+            }
+            else
+            {
+                MessageBox.Show("No ha seleccionado ningun Cliente");
+            }
+        }
+        private void LimpiarForm()
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtCedula.Clear();
+            txtDireccion.Clear();
+            txtTelefono.Clear();
+            txtEmail.Clear();
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                IdCliente = dataGridView1.CurrentRow.Cells["CLI_ID"].Value.ToString();
+                objetoCE.Id = Convert.ToInt32(IdCliente);
+                objetoCN.EliminarCliente(objetoCE);
+                MessageBox.Show("Se Eliminó correctamente el Cliente");
+                MostrarCliente();
+            }
+            else
+            {
+                MessageBox.Show("No ha seleccionado ningun Cliente");
+            }
+        }
+
+        
     }
 }
